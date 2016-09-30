e949ffd9aae7af3260250c263ba7825e52daed3a
António Nuno Monteiro <anmonteiro@gmail.com>
2016-10-01 00:04:57 +0100

    fix lint errors, handle SIGINT properly, write to readline output

--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,13 @@ const rl = readline.createInterface({
   terminal: true,
 });
 
+// eslint-disable-next-line no-use-before-define
 prompt('cljs.user=> ');
 
 // $FlowExpectedError: this is only available when CLJS artifacts are built
-const cljs = require('./main');
+const cljs = require('./main'); // eslint-disable-line import/no-unresolved
 
-function prompt(p?: string = cljs.lumo.repl.get_current_ns() + '=> ') {
+function prompt(p: string = `${cljs.lumo.repl.get_current_ns()}=> `) {
   rl.setPrompt(p);
   rl.prompt();
 }
@@ -24,16 +25,21 @@ rl.on('line', (line: string) => {
 });
 
 rl.on('close', () => {
-  console.log('crlh, close!');
+  // $FlowIssue: missing property in interface
+  rl.output.write('closing!');
 });
 
 rl.on('SIGINT', () => {
+  // $FlowIssue: missing property in interface
+  rl.output.write('\n');
+
+  readline.clearLine(process.stdout, 0);
+  readline.cursorTo(process.stdout, 0);
+
   rl.write(null, {
     ctrl: true,
-    name: 'u'
+    name: 'u',
   });
-  console.log();
-  readline.clearLine(process.stdout, 0)
-  readline.cursorTo(process.stdout, 0)
+
   prompt();
 });
