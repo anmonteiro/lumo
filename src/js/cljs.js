d660b9092809a0b71fc9775986b4ac2cdf79d41c
António Nuno Monteiro <anmonteiro@gmail.com>
2016-10-03 13:50:28 +0100

    fix typechecking & linting

--- src/js/cljs.js
+++ src/js/cljs.js
@@ -1,26 +1,36 @@
-const vm = require('vm');
+/* @flow */
 
-const cljsSrc = require('./main.js');
+const vm = require('vm');
+// $FlowExpectedError: only present after building CLJS
+const cljsSrc = require('./main.js'); // eslint-disable-line import/no-unresolved
 
 const cljsScript = new vm.Script(cljsSrc, {});
-const defaultContext = newContext();
 
-function newContext (globals) {
-  const context = globals || {module:module,require:require,process:process,console:console}
+function newContext() {
+  const context: Object = {
+    module,
+    require,
+    process,
+    console,
+  };
+
   context.global = context;
   const ctx = vm.createContext(context);
   cljsScript.runInContext(ctx);
   return ctx;
 }
 
+const defaultContext = newContext();
+
 function evalInContext(code: string) {
+  // $FlowIssue: context can have globals
   defaultContext.lumo.repl.read_eval_print_str(code);
 }
 
-function getCurrentNS(): string{
+function getCurrentNS(): string {
+  // $FlowIssue: context can have globals
   return defaultContext.lumo.repl.get_current_ns();
 }
-
 
 module.exports = {
   eval: evalInContext,
