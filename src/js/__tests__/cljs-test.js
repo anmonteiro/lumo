d7ec7c907fb1047be190c1630ab297180f89b1a4
António Nuno Monteiro <anmonteiro@gmail.com>
2016-11-04 14:58:44 +0100

    bump cljs.js test coverage to 100%

--- src/js/__tests__/cljs-test.js
+++ src/js/__tests__/cljs-test.js
@@ -3,19 +3,40 @@
 import startCLJS, * as cljs from '../cljs';
 import startREPL from '../repl';
 
+const vm = require('vm');
+
 jest.mock('../repl');
-jest.mock('../lumo');
+
+jest.mock('../lumo', () => ({
+  load: () => '',
+}));
+
+jest.mock('vm');
+
+const ctx = {
+  cljs: {
+    nodejs: {
+      enable_util_print_BANG_: () => {},
+    },
+  },
+  lumo: {
+    repl: {
+      init: () => {},
+      set_ns: () => {},
+      execute: () => {},
+      is_readable_QMARK_: () => true,
+      get_current_ns: () => 'cljs.user',
+    },
+  },
+};
+
+vm.createContext.mockImplementation(() => ctx);
 
 describe('startClojureScriptEngine', () => {
   const nextTick = process.nextTick;
 
   beforeEach(() => {
     startREPL.mockClear();
-    process.nextTick = jest.fn();
-  });
-
-  afterEach(() => {
-    process.nextTick = nextTick;
   });
 
   it('should start a REPL if opts.repl is true', () => {
@@ -26,7 +47,6 @@ describe('startClojureScriptEngine', () => {
     });
 
     expect(startREPL).toHaveBeenCalled();
-    expect(process.nextTick).toHaveBeenCalled();
   });
 
   it('returns undefined if opts.repl is false', () => {
@@ -37,8 +57,6 @@ describe('startClojureScriptEngine', () => {
     });
 
     expect(startREPL).not.toHaveBeenCalled();
-    expect(process.nextTick).not.toHaveBeenCalled();
-
     expect(ret).toBeUndefined();
 
     startREPL.mockClear();
@@ -51,7 +69,6 @@ describe('startClojureScriptEngine', () => {
 
     expect(startREPL).not.toHaveBeenCalled();
     expect(ret2).toBeUndefined();
-    expect(process.nextTick).not.toHaveBeenCalled();
   });
 
   it('calls `executeScript` and bails if there\'s a main opt', () => {
@@ -62,7 +79,6 @@ describe('startClojureScriptEngine', () => {
     });
 
     expect(startREPL).not.toHaveBeenCalled();
-    expect(process.nextTick).not.toHaveBeenCalled();
   });
 
   it('doesn\'t init the CLJS engine if it already started', () => {
@@ -73,8 +89,66 @@ describe('startClojureScriptEngine', () => {
       scripts: [['text', ':foo'], ['path', 'foo.cljs']],
     });
 
-    expect(process.nextTick).not.toHaveBeenCalled();
     expect(startREPL).toHaveBeenCalled();
+  });
+
+  describe('in development', () => {
+    beforeEach(() => {
+      vm.createContext.mockClear();
+      // eslint-disable-next-line arrow-parens
+      process.nextTick = jest.fn((f: Function) => f());
+    });
+
+    afterEach(() => {
+      process.nextTick = nextTick;
+    });
+
+    it('creates and returns a vm context', () => {
+      startCLJS({
+        repl: true,
+        _: [],
+        scripts: [],
+      });
+
+      expect(vm.createContext).toHaveBeenCalled();
+      expect(vm.createContext.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('in production', () => {
+    let startClojureScriptEngine;
+
+    beforeEach(() => {
+      jest.resetModules();
+      // eslint-disable-next-line arrow-parens
+      process.nextTick = jest.fn((f: Function) => f());
+
+      Object.assign(global, {
+        initialize: jest.fn(),
+        __DEV__: false,
+      }, ctx);
+      // eslint-disable-next-line global-require
+      startClojureScriptEngine = require('../cljs').default;
+    });
+
+    afterEach(() => {
+      for (const key of Object.keys(ctx).concat(['initialize'])) {
+        Reflect.deleteProperty(global, key);
+      }
+      __DEV__ = true;
+      process.nextTick = nextTick;
+    });
+
+    it('calls the global initialize function', () => {
+      startClojureScriptEngine({
+        repl: true,
+        _: [],
+        scripts: [],
+      });
+
+      // eslint-disable-next-line no-undef
+      expect(initialize).toHaveBeenCalled();
+    });
   });
 });
 
