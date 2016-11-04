b89a43e1786952ceb3a8d3a11a6e29c57ffe2856
António Nuno Monteiro <anmonteiro@gmail.com>
2016-11-04 20:35:39 +0100

    improve repl test coverage

--- src/js/__tests__/repl-test.js
+++ src/js/__tests__/repl-test.js
@@ -1,17 +1,46 @@
 /* @flow */
 
-import startREPL from '../repl';
-import replHistory from '../replHistory';
+// functions are hoisted, calling this before defining for readability
+// eslint-disable-next-line no-use-before-define
+mockReplHistory();
+const replHistory = require('../replHistory');
+
+let startREPL = require('../repl').default;
 
 const setPrompt = jest.fn();
 const prompt = jest.fn();
-const on = jest.fn();
+let on;
 
-jest.mock('../replHistory', () => jest.fn(() => ({
-  setPrompt,
-  prompt,
-  on,
-})));
+function genOn(line: ?string = null) {
+  on = jest.fn((type: string, f: Function) => {
+    switch (type) {
+      case 'line': return f(line || '(+ 1 2)');
+      case 'SIGINT': return f();
+      default: return undefined;
+    }
+  });
+  return on;
+}
+
+function mockReplHistory(line?: string) {
+  jest.mock('../replHistory', () => jest.fn(() => ({
+    setPrompt,
+    prompt,
+    on: genOn(line),
+    output: {
+      write: jest.fn(),
+    },
+    write: jest.fn(),
+  })));
+}
+
+jest.mock('readline');
+jest.mock('../cljs', () => ({
+  // eslint-disable-next-line arrow-parens
+  isReadable: jest.fn((input: string) => ''),
+  execute: jest.fn(),
+  getCurrentNamespace: jest.fn(() => 'cljs.user'),
+}));
 
 describe('startREPL', () => {
   beforeEach(() => {
@@ -38,5 +67,69 @@ describe('startREPL', () => {
     expect(onCalls.length).toBe(2);
     expect(onCalls[0][0]).toBe('line');
     expect(onCalls[1][0]).toBe('SIGINT');
+  });
+
+  describe('calls process.exit if an exit command is specified', () => {
+    const exit = process.exit;
+
+    beforeEach(() => {
+      process.exit = jest.fn();
+      jest.resetModules();
+    });
+
+    afterAll(() => {
+      process.exit = exit;
+    });
+
+    it(':cljs/quit', () => {
+      mockReplHistory(':cljs/quit');
+      // eslint-disable-next-line global-require
+      startREPL = require('../repl').default;
+
+      startREPL({});
+
+      expect(process.exit).toHaveBeenCalled();
+    });
+
+    it('exit', () => {
+      mockReplHistory('exit');
+      // eslint-disable-next-line global-require
+      startREPL = require('../repl').default;
+
+      startREPL({});
+
+      expect(process.exit).toHaveBeenCalled();
+    });
+  });
+
+  describe('emits the correct prompt according to the line', () => {
+    beforeAll(() => {
+      jest.resetModules();
+      mockReplHistory();
+      // eslint-disable-next-line global-require
+      startREPL = require('../repl').default;
+      jest.clearAllMocks();
+    });
+
+    it('should emit the primary prompt after input is completely readable', () => {
+      startREPL({});
+
+      expect(setPrompt).toHaveBeenCalledWith('cljs.user=> ');
+    });
+
+    it('should emit the secondary prompt', () => {
+      jest.resetModules();
+      jest.mock('../cljs', () => ({
+        // eslint-disable-next-line arrow-parens
+        isReadable: jest.fn((input: string) => false),
+        execute: jest.fn(),
+        getCurrentNamespace: jest.fn(() => 'cljs.user'),
+      }));
+      // eslint-disable-next-line global-require
+      startREPL = require('../repl').default;
+
+      startREPL({});
+      expect(setPrompt).toHaveBeenCalledWith('       #_=> ');
+    });
   });
 });
