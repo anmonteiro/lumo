98bc7e505d9f1671627be83b8a6bd9eb5de9d19f
Nicolas Peckman <npeckman@extron.com>
2016-11-21 15:48:44 -0500

    Initial commit of working lumo.classpath, a namespace for interacting with the classpath in dynamic ways

--- src/cljs/lumo/classpath.cljs
+++ src/cljs/lumo/classpath.cljs
@@ -1,8 +1,47 @@
 (ns lumo.classpath
-  (:require [cljs.nodejs :as nodejs]))
+  (:require [cljs.nodejs :as nodejs]
+            [clojure.string :as string]))
 
 (def fs (nodejs/require "fs"))
 
-(defn ^:export filenames
-  [cp]
-  (map #(. fs readdirSync %) cp))
+(defn directory?
+  [path]
+  (. (. fs statSync path) isDirectory))
+
+(defn file?
+  [path]
+  (and (. (. fs statSync path) isFile) (or (string/ends-with? path ".cljs")
+                                           (string/ends-with? path ".cljc"))))
+
+(defn jarfile?
+  [path]
+  (string/ends-with? path ".jar"))
+
+(defn filenames
+  [path]
+  (let [root (js->clj (. fs readdirSync path))
+        root-files (filter #(file? (str path "/" %)) root)
+        sub-dirs (map #(str path "/" %) (filter #(directory? (str path "/" %)) root))
+        sub-files (map filenames sub-dirs)
+        ]
+    (flatten [root-files sub-files])))
+
+(defn ^:export classpath
+  []
+  (js/LUMO_READ_SOURCES))
+
+(defn ^:export classpath-files
+  []
+  (flatten (map filenames (classpath))))
+
+(defn ^:export classpath-jarfiles
+  []
+  (filter jarfile? (classpath)))
+
+(defn ^:export add-source!
+  [path]
+  (js/LUMO_ADD_SOURCES #js [path]))
+
+(defn ^:export remove-source!
+  [path]
+  (js/LUMO_REMOVE_SOURCE path))
