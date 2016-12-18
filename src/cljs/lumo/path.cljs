b50571b1d61c10f15424027fad11ee8131b92677
jjl <james@behemoth>
2016-12-18 14:16:30 +0100

    Fix docstrings

--- src/cljs/lumo/path.cljs
+++ src/cljs/lumo/path.cljs
@@ -8,61 +8,74 @@
 (def sep (.-sep js-path))
 
 (defn basename
-  "returns the basename (file without directory) of the path
+  "Returns the basename (file without directory) of the path
    args: path
    return: string"
   [path]
   (.basename js-path path))
 
 (defn dirname
-  "returns the dirname of the path
-   args: path
+  "Returns the dirname of the path
+   args: [path]
    return: string"
   [path]
   (.dirname js-path path))
 
 (defn extension
-  "returns the extension of the file path
-   args: path
+  "Returns the extension of the file path (dot included)
+   args: [path]
    return: string"
   [path]
   (.extname js-path path))
 
 (defn format
-  ""
+  "Turns a map of the form returned by `parse` into a string
+   args [map]
+   returns: string"
+
   [{:keys [dir root base name ext]}]
   (.format js-path #js {:dir dir :root root :base base :name name :ext ext}))
 
 (defn absolute?
-  ""
+  "true if path is absolute
+   args: [path]
+   returns: bool"
   [path]
   (.isAbsolute js-path path))
 
 (defn join
-  ""
+  "Joins path segments together
+   args: [& segments]
+   returns: string"
   [& ps]
   (i/js-apply (.-join js-path) nil ps))
 
 (defn normalize
-  ""
+  "Resolves . and .. segments of path
+   args: [path]
+   returns: string"
   [path]
   (.normalize js-path path))
 
 (defn parse
-  ""
+  "Returns a map describing the file path
+   args: [path]
+   returns: map with keys (all string values):
+     :root :dir :base :ext :name"
   [path]
   (i/obj->map (.parse js-path path) true))
 
 (defn relative
-  ""
+  "The relative path from `from` to `to`
+   args: [from to]
+   returns: string"
   [from to]
   (.relative js-path from to))
 
 (defn resolve
-  ""
+  "Joins the given path segments and absolutifies
+   args [& segments]
+   returns: string"
   [& ps]
   (i/js-apply (.-resolve js-path) nil ps))
-
-
-  
 
--- src/cljs/lumo/path/windows.cljs
+++ src/cljs/lumo/path/windows.cljs
@@ -5,35 +5,72 @@
 (def win32 (.-win32 (js/require "path")))
 
 (defn basename
-  "returns the basename of the path
+  "Returns the basename (file without directory) of the path
    args: path
    return: string"
   [path]
   (.basename win32 path))
 
-(defn dirname [path]
+(defn dirname
+  "Returns the dirname of the path
+   args: [path]
+   return: string"
+  [path]
   (.dirname win32 path))
 
-(defn extension [path]
+(defn extension
+  "Returns the extension of the file path (dot included)
+   args: [path]
+   return: string"
+  [path]
   (.extname win32 path))
 
-(defn format [{:keys [dir root base name ext]}]
+(defn format
+  "Turns a map of the form returned by `parse` into a string
+   args [map]
+   returns: string"
+  [{:keys [dir root base name ext]}]
   (.format win32 #js {:dir dir :root root :base base :name name :ext ext}))
 
-(defn absolute? [path]
+(defn absolute?
+  "true if path is absolute
+   args: [path]
+   returns: bool"
+  [path]
   (.isAbsolute win32 path))
 
-(defn join [& ps]
+(defn join
+  "Joins path segments together
+   args: [& segments]
+   returns: string"
+  [& ps]
   (i/js-apply (.-join win32) nil ps))
 
-(defn normalize [path]
+(defn normalize
+  "Resolves . and .. segments of path
+   args: [path]
+   returns: string"
+  [path]
   (.normalize win32 path))
 
-(defn parse [path]
+(defn parse
+  "Returns a map describing the file path
+   args: [path]
+   returns: map with keys (all string values):
+     :root :dir :base :ext :name"
+  [path]
   (i/obj->map (.parse win32 path) true))
 
-(defn relative [from to]
+(defn relative
+  "The relative path from `from` to `to`
+   args: [from to]
+   returns: string"
+  [from to]
   (.relative win32 from to))
 
-(defn resolve [& ps]
+(defn resolve
+  "Joins the given path segments and absolutifies
+   args [& segments]
+   returns: string"
+  [& ps]
   (i/js-apply (.-resolve win32) nil ps))
