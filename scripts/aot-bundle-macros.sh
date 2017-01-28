fae22c1b8bc36de66aaca889d7470a278b0d9858
António Nuno Monteiro <anmonteiro@gmail.com>
2017-01-27 20:27:07 -0800

    Upgrade to the latest ClojureScript release (#71)

--- scripts/aot-bundle-macros.sh
+++ scripts/aot-bundle-macros.sh
@@ -14,7 +14,7 @@ echo "### Compiling Macro Namespaces"
 mkdir -p lumo-cljs/out/macros-tmp
 
 $(pwd)/build/lumo --quiet -c target -sdk lumo-cljs/out/macros-tmp <<REPL_INPUT
-(require-macros 'lumo.repl 'clojure.template 'cljs.spec 'cljs.spec.impl.gen 'cljs.test)
+(require-macros 'lumo.repl 'clojure.template 'cljs.spec 'cljs.spec.impl.gen 'cljs.test 'cljs.reader)
 REPL_INPUT
 
 mv lumo-cljs/out/macros-tmp/lumo_SLASH_repl\$macros.js target/lumo/repl\$macros.js
@@ -23,6 +23,8 @@ mv lumo-cljs/out/macros-tmp/clojure_SLASH_template\$macros.js target/clojure/tem
 mv lumo-cljs/out/macros-tmp/clojure_SLASH_template\$macros.cache.json target/clojure/template\$macros.cache.json
 mv lumo-cljs/out/macros-tmp/cljs_SLASH_test\$macros.js target/cljs/test\$macros.js
 mv lumo-cljs/out/macros-tmp/cljs_SLASH_test\$macros.cache.json target/cljs/test\$macros.cache.json
+mv lumo-cljs/out/macros-tmp/cljs_SLASH_reader\$macros.js target/cljs/reader\$macros.js
+mv lumo-cljs/out/macros-tmp/cljs_SLASH_reader\$macros.cache.json target/cljs/reader\$macros.cache.json
 mv lumo-cljs/out/macros-tmp/cljs_SLASH_spec\$macros.js target/cljs/spec\$macros.js
 mv lumo-cljs/out/macros-tmp/cljs_SLASH_spec\$macros.cache.json target/cljs/spec\$macros.cache.json
 mv lumo-cljs/out/macros-tmp/cljs_SLASH_spec_SLASH_impl_SLASH_gen\$macros.js target/cljs/spec/impl/gen\$macros.js
--- scripts/prepare_snapshot.js
+++ scripts/prepare_snapshot.js
@@ -4,4 +4,4 @@ const x = fs.readFileSync('target/main.js', 'utf8');
 
 const str = x.replace(/(cljs\.nodejs={};[\s\S]+?a;return b}\(\);return null};)/, 'this.initialize=(function(){$1});');
 
-fs.writeFileSync('target/main.js', str.replace(/var boot={cljs:{}};[\s\S]+?not set"\);/, ''), 'utf8');
+fs.writeFileSync('target/main.js', str.replace(/var boot={cljs:{}};[\s\S]+?cljs.core.drop.cljs\$core\$IFn\$_invoke\$arity\$2\(2,process.argv\)\);/, ''), 'utf8');
