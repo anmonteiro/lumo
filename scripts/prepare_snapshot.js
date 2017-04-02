27a5cda20020fedb3c743842177a2a7c372aecdd
António Nuno Monteiro <anmonteiro@gmail.com>
2017-04-02 16:55:26 -0700

    more script fixes

--- scripts/prepare_snapshot.js
+++ scripts/prepare_snapshot.js
@@ -2,15 +2,16 @@ const fs = require('fs');
 
 const x = fs.readFileSync('target/main.js', 'utf8');
 
+// prettier-ignore
 const str = x
   .replace(
     /(cljs\.nodejs={};[\s\S]+?a;return b}\(\);return null};)/,
-    'this.initialize=(function(){$1});',
+    'this.initialize=(function(){$1});'
   )
   .replace(/var boot={cljs:{}};boot.cljs.*?={};/, '')
   .replace(
     /cljs.nodejscli={};cljs.core.truth_\(COMPILED\)[\s\S]+?cljs.core.drop.cljs\$core\$IFn\$_invoke\$arity\$2\(2,process.argv\)\);/,
-    '',
+    ''
   );
 
 fs.writeFileSync('target/main.js', str, 'utf8');
