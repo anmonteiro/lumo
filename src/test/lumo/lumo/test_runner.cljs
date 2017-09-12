1fa89ba373d3f6d28550d5199093cd6a262b5253
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2017-09-11 22:25:27 -0700

    util tests

--- src/test/lumo/lumo/test_runner.cljs
+++ src/test/lumo/lumo/test_runner.cljs
@@ -3,11 +3,13 @@
             lumo.js-deps-tests
             lumo.repl-tests
             lumo.closure-tests
-            lumo.build-api-tests))
+            lumo.build-api-tests
+            lumo.util-tests))
 
 (defn run-all-tests []
   (run-tests
     'lumo.repl-tests
     'lumo.js-deps-tests
     'lumo.closure-tests
-    'lumo.build-api-tests))
+    'lumo.build-api-tests
+    'lumo.util-tests))
